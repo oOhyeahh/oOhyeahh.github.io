ad2a6036a4710b4b98b6f6b58ae5567b03c37d42
oOhyeahh <oliverye9606@gmail.com>
2018-02-25 14:18:29 +1100

    fix react

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/app.css';
+import ProfilePage from './APP';
+
+ReactDOM.render(<ProfilePage />, document.getElementById('root'));
