2d511b27d63c3dc5d9b7b0efcd53f8a06491fe25
oOhyeahh <oliverye9606@gmail.com>
2018-08-28 10:51:26 +1000

    remove react component

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './styles/app.css';
-import ProfilePage from './APP';
-
-ReactDOM.render(<ProfilePage />, document.getElementById('root'));
